b65b4f3e0d0411a4e1e888cf0fc5c96718540ef2
Josh Hills <josh.hills@jagex.com>
2020-02-04 03:59:27 +0000

    chore(master): fixing port

--- src/config/application.ts
+++ src/config/application.ts
@@ -4,7 +4,7 @@
  */
 export class ApplicationConfig {
     // The port to run the web-server on.
-    public static readonly PORT = 3000 || process.env.PORT;
+    public static readonly PORT = process.env.PORT || 3000;
     
     // The maximum number of hiscores to track at a given time.
     public static readonly MAX_NUM_HISCORES = 5;
